530fa1c1f15b8e550d81c07c2a8b9de6a75de3a6
Bruno Menezes <brunodmenezes@gmail.com>
2013-07-07 01:06:47 -0300

    melhorando legibilidade

--- web-app/js/grails-angular-template.js
+++ web-app/js/grails-angular-template.js
@@ -1,34 +1,35 @@
-var gatModule = angular.module('gatLoader', [])
-	.factory('templateService', ['$http', '$rootScope', '$compile', function($http, $rootScope, $compile){
-		var url = config.contextPath + "/trader/templateLoader?templateName="; 
-		var templateService = new function(){
-			var self = this;
-			var _resourceLocation, _selector, _data;
-			this.load = function(url){
-				_resourceLocation = url;
-				return self;
-			}
+var module = angular.module('TemplateLoader', []);
+	module.factory('templateService', ['$http', '$rootScope', '$compile', 
+		function($http, $rootScope, $compile){
+			var url = config.contextPath + "/trader/templateLoader?templateName="; 
+			var templateService = new function(){
+				var self = this;
+				var _resourceLocation, _selector, _data;
+				this.load = function(url){
+					_resourceLocation = url;
+					return self;
+				}
 
-			this.into = function(sel){
-				_selector = sel;
-				return self;
-			}
+				this.into = function(sel){
+					_selector = sel;
+					return self;
+				}
 
-			this.withData = function(data){
-				_data = data;
-				return self;	
-			}
+				this.withData = function(data){
+					_data = data;
+					return self;	
+				}
 
-			this.start = function(){
-				$http.get(url + _resourceLocation).success(function(htmlPiece){
-					var templateZone = angular.element(_selector);
-				  	var scope = _data.scope? _data.scope : $rootScope.$new();
-			    	var compiledTemplate = $compile(htmlPiece)(scope);
-			    	templateZone.html(compiledTemplate);
-				    $rootScope.$broadcast(_resourceLocation, _data);
-				});
+				this.start = function(){
+					$http.get(url + _resourceLocation).success(function(htmlPiece){
+						var templateZone = angular.element(_selector);
+					  	var scope = _data.scope? _data.scope : $rootScope.$new();
+				    	var compiledTemplate = $compile(htmlPiece)(scope);
+				    	templateZone.html(compiledTemplate);
+					    $rootScope.$broadcast(_resourceLocation, _data);
+					});
+				}
 			}
+			return templateService;
 		}
-
-	return templateService;
-}]);
+	]);
