0a390804cb377b61295d0e2bbdc560af624f321c
Bruno Menezes <brunodmenezes@gmail.com>
2013-07-19 22:34:05 -0300

    fix description

--- GrailsAngularTemplateGrailsPlugin.groovy
+++ GrailsAngularTemplateGrailsPlugin.groovy
@@ -5,7 +5,7 @@ class GrailsAngularTemplateGrailsPlugin {
     def title = "Grails Angular Template Plugin"
     def author = "Bruno Menezes"
     def authorEmail = "brunodmenezes@icloud.com"
-    def description = "The best way to load GSP templates with angular build-in structures, using the steady Grails mechanism of template load.'
+    def description = "The best way to load GSP templates with angular build-in structures, using the steady Grails mechanism of template load."
     def organization = [ name: "KDW-Tecnologia Informática LTDA", url:'']
     def developers = [ [ name: "Fábio Oshiro", email: "fabio.oshiro@gmail.com" ]]
     def documentation = "https://github.com/brunomenezes/grails-angular-template"
